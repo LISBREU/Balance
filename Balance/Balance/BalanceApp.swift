dfc6f292258bde637e153fb8b64b51a77fce0195
LISBREU <ilnurshabanov79@gmail.com>
2024-02-08 20:16:55 +0300

    start project

---
+++ Balance/Balance/BalanceApp.swift
@@ -0,0 +1,17 @@
+//
+//  BalanceApp.swift
+//  Balance
+//
+//  Created by Ilnur Shabanov on 08.02.2024.
+//
+
+import SwiftUI
+
+@main
+struct BalanceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
