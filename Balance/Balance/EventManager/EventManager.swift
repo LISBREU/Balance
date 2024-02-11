cbde0d5b80e8c7da8f9fda478aed6eb97d4ee42b
Janiecee <21466184@sberbank.ru>
2024-02-11 16:21:58 +0300

    Добавлен список событий

---
+++ Balance/Balance/EventManager/EventManager.swift
@@ -0,0 +1,18 @@
+//
+//  EventManager.swift
+//  Balance
+//
+//  Created by Ляхевич Александр Олегович on 11.02.2024.
+//
+
+import SwiftUI
+
+struct EventManager: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    EventManager()
+}
---
+++ Balance/Balance/EventManager/EventsView.swift
@@ -0,0 +1,18 @@
+//
+//  EventsView.swift
+//  Balance
+//
+//  Created by Ляхевич Александр Олегович on 11.02.2024.
+//
+
+import SwiftUI
+
+struct EventsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    EventsView()
+}
